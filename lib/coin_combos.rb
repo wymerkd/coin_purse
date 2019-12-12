f142a3bd1df325cd75c42ce1a4180ae3a2f22b5d
Student <example@example.com>
2019-12-12 11:29:16 -0800

    add test for quarted and make it pass

--- lib/coin_combos.rb
+++ lib/coin_combos.rb
@@ -1,1 +1,48 @@
-Class
+
+class Pocket
+  attr_reader(:coin, :pennies, :nickles, :dimes, :quarters)
+
+  def initialize(coin)
+    @coin = coin
+    @pennies = 0
+    @nickles = 0
+    @dimes = 0
+    @quarters = 0
+  end
+
+  def coin_hash
+    until (@coin === 0)
+      if @coin >= 25 && @coin % 25 === 0
+          @coin -= 25
+          @quarters += 1
+        elsif @coin % 10
+          @coin -= 10
+          @dimes += 1
+        end
+      end
+      purse = []
+      purse.push(@quarters)
+      purse.push("quarters")
+      purse
+    end
+
+  end
+
+  # @coin_purse = []
+
+  #   coin_word = {1 => "penny", 2 => "penny", 3 => "penny", 4 => "penny", 5 => "nickel", 10 => "dime", 25 => "quarter"}
+  #   x = 0
+  #   array = []
+    # until (x >= @coin )
+    #   x = x + 1
+  #     fetch_coin = coin_word.fetch(@coin)
+  #     if fetch_coin === "nickel"
+  #       @coin = @coin - 5
+  #       array.push(fetch_coin)
+  #     elsif @coin < 5
+  #       @coin = @coin -1
+  #       array.push(fetch_coin)
+  #     end
+  #   end
+  #   array
+  # end
--- spec/coin_combos_spec.rb
+++ spec/coin_combos_spec.rb
@@ -1,8 +1,13 @@
 require('rspec')
 require('coin_combos')
 
-describe('#coin_maker') do
-  it("returns number of pennies depending on users input") do
-    expect(coin_maker(1)).to(eq("penny"))
+describe('#coin_hash') do
+  it("returns array of coin names") do
+    test = Pocket.new(50)
+    expect(test.coin_hash()).to(eq([2,"quarters"]))
   end
+  # it("returns number of pennies depending on users input") do
+  #   test = Pocket.new(45)
+  #   expect(test.coin_hash()).to(eq(2))
+  # end
 end
