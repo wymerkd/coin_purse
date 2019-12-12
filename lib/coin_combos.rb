dc77f643dfbe0fa252017c7fd9ad462d322fb31d
Student <example@example.com>
2019-12-12 13:39:46 -0800

    add functionality to account for pennies and nickles

--- lib/coin_combos.rb
+++ lib/coin_combos.rb
@@ -1,62 +1,81 @@
 
+
 class Pocket
-  attr_reader(:coin, :pennies, :nickles, :dimes, :quarters)
+  attr_reader(:coin, :pennies, :nickels, :dimes, :quarters)
 
   def initialize(coin)
     @coin = coin
     @pennies = 0
-    @nickles = 0
+    @nickels = 0
     @dimes = 0
     @quarters = 0
+    @purse = []
   end
 
   def coin_hash
-    until (@coin === 0)
-      if @coin >= 25 && @coin % 25 === 0
-          @coin -= 25
-          @quarters += 1
-        elsif @coin % 10 === 0 && @coin != 0
-          puts @coin
-          @coin -= 10
-          @dimes += 1
-        end
+    until (@coin == 0)
+      if @coin >= 25
+        @coin -= 25
+        @quarters += 1
+      elsif @coin % 10 == 0 && @coin >= 10
+        @coin -= 10
+        @dimes += 1
+      elsif @coin % 5 == 0
+        @coin -= 5
+        @nickels += 1
+      else
+        @coin -= 1
+        @pennies += 1
       end
-      purse = []
-      if @quarters > 1
-      purse.push(@quarters)
-      purse.push("quarters")
+    end
+    if @quarters > 1
+      @purse.push(@quarters)
+      @purse.push("quarters")
     elsif @quarters == 1
-      purse.push(@quarters)
-      purse.push("quarter")
+      @purse.push(@quarters)
+      @purse.push("quarter")
     end
     if @dimes > 1
-    purse.push(@dimes)
-    purse.push("dimes")
-  elsif @dimes == 1
-    purse.push(@dimes)
-    purse.push("dime")
-  end
-    puts purse
-    purse
+      @purse.push(@dimes)
+      @purse.push("dimes")
+    elsif @dimes == 1
+      @purse.push(@dimes)
+      @purse.push("dime")
     end
-
+    if @nickels > 1
+      @purse.push(@nickels)
+      @purse.push("nickels")
+    elsif @nickels == 1
+      @purse.push(@nickels)
+      @purse.push("nickel")
+    end
+    if @pennies > 1
+      @purse.push(@pennies)
+      @purse.push("pennies")
+    elsif @pennies == 1
+      @purse.push(@pennies)
+      @purse.push("penny")
+    end
+    purse_string = @purse.join(" ")
+    purse_string
   end
+end
 
-  # @coin_purse = []
+# @coin_purse = []
 
-  #   coin_word = {1 => "penny", 2 => "penny", 3 => "penny", 4 => "penny", 5 => "nickel", 10 => "dime", 25 => "quarter"}
-  #   x = 0
-  #   array = []
-    # until (x >= @coin )
-    #   x = x + 1
-  #     fetch_coin = coin_word.fetch(@coin)
-  #     if fetch_coin === "nickel"
-  #       @coin = @coin - 5
-  #       array.push(fetch_coin)
-  #     elsif @coin < 5
-  #       @coin = @coin -1
-  #       array.push(fetch_coin)
-  #     end
-  #   end
-  #   array
-  # end
+#   coin_word = {1 => "penny", 2 => "penny", 3 => "penny", 4 => "penny", 5 => "nickel", 10 => "dime", 25 => "quarter"}
+#   x = 0
+#   array = []
+# until (x >= @coin )
+#   x = x + 1
+#     fetch_coin = coin_word.fetch(@coin)
+#     if fetch_coin === "nickel"
+#       @coin = @coin - 5
+#       array.push(fetch_coin)
+#     elsif @coin < 5
+#       @coin = @coin -1
+#       array.push(fetch_coin)
+#     end
+#   end
+#   array
+# end
--- spec/coin_combos_spec.rb
+++ spec/coin_combos_spec.rb
@@ -4,10 +4,26 @@ require('coin_combos')
 describe('#coin_hash') do
   it("returns array of coin names") do
     test = Pocket.new(50)
-    expect(test.coin_hash()).to(eq([2,"quarters"]))
+    expect(test.coin_hash()).to(eq("2 quarters"))
   end
-  # it("returns number of pennies depending on users input") do
-  #   test = Pocket.new(45)
-  #   expect(test.coin_hash()).to(eq([2,"dimes",1,"quarter"]))
-  # end
+  it("returns number of pennies depending on users input") do
+    test = Pocket.new(35)
+    expect(test.coin_hash()).to(eq("1 quarter 1 dime"))
+  end
+  it("returns number of pennies depending on users input") do
+    test = Pocket.new(45)
+    expect(test.coin_hash()).to(eq("1 quarter 2 dimes"))
+  end
+  it("returns number of pennies depending on users input") do
+    test = Pocket.new(80)
+    expect(test.coin_hash()).to(eq("3 quarters 1 nickel"))
+  end
+  it("returns number of pennies depending on users input") do
+    test = Pocket.new(111)
+    expect(test.coin_hash()).to(eq("4 quarters 1 dime 1 penny"))
+  end
+  it("returns number of pennies depending on users input") do
+    test = Pocket.new(11105)
+    expect(test.coin_hash()).to(eq("444 quarters 1 nickel"))
+  end
 end
