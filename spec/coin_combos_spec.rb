b971396f561089e5b97b6c1758fdc09734c27f3d
Student <example@example.com>
2019-12-12 09:05:48 -0800

    add first test, for returing pennies

--- spec/coin_combos_spec.rb
+++ spec/coin_combos_spec.rb
@@ -1,4 +1,8 @@
 require('rspec')
 require('coin_combos')
 
-describe('#')'
+describe('#coin_maker') do
+  it("returns number of pennies depending on users input") do
+    expect(coin_maker(1)).to(eq("penny"))
+  end
+end
